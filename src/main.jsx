2d3175a52b4c69813e9eb9b7012238bfd3ade5cd
kalethabh <kalethabh@gmail.com>
2023-07-18 23:52:12 -0500

    Portafolio

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={"https://kaleth-dev.vercel.app/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
